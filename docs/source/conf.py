5aeeb5fb1e4e69c2a5c1a8bc87786944c197c02a
Jacob Hirschi <hirschij@oregonstate.edu>
2023-12-04 11:34:34 -0800

    added docs from sphinx-quickstart run in docs/

---
+++ docs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Testing Restructured Text Capabilities'
+copyright = '2023, Stannis of the House Baratheon'
+author = 'Stannis of the House Baratheon'
+release = 'v0.0.23'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,21 @@
+
+ReStructured Text and Sphinx
+============================
+This is a test of restructured text and Sphinx.
+Linux!
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+How does it work?
+-----------------
+* Works good
+* or at least it seems to work well
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
