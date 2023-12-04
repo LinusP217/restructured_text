f68a19ae1b64ad91eb9bc56b345cc781dba99f44
Jacob Hirschi <hirschij@oregonstate.edu>
2023-12-04 10:08:26 -0800

    greeting python scripts added to the thingy

---
+++ greeting.py
@@ -0,0 +1,9 @@
+
+def generic_greeting():
+    ''' Prints a generic meeting message.'''
+    print("Hello, World!")
+
+def specific_greeting():
+    ''' Prints a personalized meeting message.'''
+    name = input("What is your name?\n")
+    print(f"Hello, {name}")
